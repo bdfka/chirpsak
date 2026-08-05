10d2407924c39aaf20324d1075ed7e47a9a66be4
bdfka <bdfyjd_TV@mail.ru>
2026-08-05 10:27:35 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<path:filename>')
+def serve(filename='index.html'):
+    return send_from_directory('.', filename)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
